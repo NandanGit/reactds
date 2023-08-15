ebc9ea586bd53809abd408cfdd7d252c00e8a52a
Nandan Reddy <nandan.reddy1133@gmail.com>
2023-08-15 20:27:16 +0530

    Add prettier config

--- packages/coorg/src/components/Button/Button.stories.ts
+++ packages/coorg/src/components/Button/Button.stories.ts
@@ -1,41 +1,41 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { Button } from './Button';
+import type {Meta, StoryObj} from '@storybook/react';
+import {Button} from './Button';
 
 const meta = {
-	title: 'Components/Button',
-	component: Button,
-	parameters: {
-		layout: 'fullscreen',
-	},
-	tags: ['autodocs'],
+  title: 'Components/Button',
+  component: Button,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  tags: ['autodocs'],
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-	args: {
-		children: 'Button',
-	},
+  args: {
+    children: 'Button',
+  },
 };
 
 export const Variant: Story = {
-	args: {
-		variant: 'outline',
-		children: 'Button',
-	},
+  args: {
+    variant: 'outline',
+    children: 'Button',
+  },
 };
 
 export const Intent: Story = {
-	args: {
-		intent: 'secondary',
-		children: 'Button',
-	},
+  args: {
+    intent: 'secondary',
+    children: 'Button',
+  },
 };
 
 export const Size: Story = {
-	args: {
-		size: 'sm',
-		children: 'Button',
-	},
+  args: {
+    size: 'sm',
+    children: 'Button',
+  },
 };
