4b11181a2c6f2bde396714968f801ec13e86f1b8
Nandan Reddy <nandan.reddy1133@gmail.com>
2023-08-17 02:28:43 +0530

    Add prepack scripts

--- packages/coorg/rollup.config.dev.mjs
+++ packages/coorg/rollup.config.dev.mjs
@@ -1,12 +1,9 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
 import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
-import shell from 'rollup-plugin-shell';
-import fs from 'fs';
 
 import packageJson from './package.json' assert { type: 'json' };
 
