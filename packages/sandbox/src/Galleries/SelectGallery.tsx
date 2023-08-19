2474efee3fe4a7b49916b98e9705afe7d8f59bb9
Nandan Reddy <nandan.reddy1133@gmail.com>
2023-08-19 23:41:32 +0530

    Update SelectGallery.tsx

--- packages/sandbox/src/Galleries/SelectGallery.tsx
+++ packages/sandbox/src/Galleries/SelectGallery.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import { Container } from '../Container';
+import { Select } from '@reactds/coorg';
 
 export interface SelectGalleryProps
 	extends React.HTMLAttributes<HTMLDivElement> {}
 
 export const SelectGallery: React.FC<SelectGalleryProps> = () => {
-	return <Container>{SelectGallery.displayName}</Container>;
+	return (
+		<Container>
+			<Select>
+				<Select.Item value='dark'>Dark</Select.Item>
+				<Select.Item value='light'>Light</Select.Item>
+				<Select.Item value='system'>System</Select.Item>
+			</Select>
+			<Select.Root>
+				<Select.Trigger>
+					<Select.Value placeholder='Theme' />
+				</Select.Trigger>
+				<Select.Content>
+					<Select.Item value='dark'>Dark</Select.Item>
+					<Select.Item value='light'>Light</Select.Item>
+					<Select.Item value='system'>System</Select.Item>
+				</Select.Content>
+			</Select.Root>
+		</Container>
+	);
 };
 
 SelectGallery.displayName = 'SelectGallery';
