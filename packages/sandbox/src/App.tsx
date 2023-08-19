4ee41baa36d9984df8a57949c33c43676fe6650f
Nandan Reddy <nandan.reddy1133@gmail.com>
2023-08-19 15:46:16 +0530

    Update Sandbox

--- packages/sandbox/src/App.tsx
+++ packages/sandbox/src/App.tsx
@@ -1,12 +1,39 @@
 import './App.css';
-import { Button } from '@reactds/coorg';
+import { Button, Surface } from '@reactds/coorg';
 
 function App() {
 	return (
 		<>
-			<Button variant='outline' onClick={() => console.log('Clicked')}>
-				Hello
-			</Button>
+			<Surface
+				style={{
+					minWidth: '80%',
+					height: '80%',
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					gap: '1rem',
+				}}
+			>
+				<Button
+					// intent='info'
+					// variant='filled'
+					// disabled
+					onClick={() => console.log('Clicked')}
+				>
+					Hello
+				</Button>
+			</Surface>
+
+			{/* Background */}
+			<div className='background'>
+				<div className='animation-container'>
+					<div className='gradient gradient-1' />
+					<div className='gradient gradient-2' />
+					<div className='gradient gradient-3' />
+					<div className='gradient gradient-4' />
+					<div className='gradient gradient-5' />
+				</div>
+			</div>
 		</>
 	);
 }
