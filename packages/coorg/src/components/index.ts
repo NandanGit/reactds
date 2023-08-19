50d9e3933ef493b44549cbaccf08e4a8c6a73ca1
Nandan Reddy <nandan.reddy1133@gmail.com>
2023-08-19 23:41:27 +0530

    Update index.ts

--- packages/coorg/src/components/index.ts
+++ packages/coorg/src/components/index.ts
@@ -1,2 +1,6 @@
-export { Button, type ButtonProps } from './Button/Button';
+// UI
 export { Surface, type SurfaceProps } from './Surface/Surface';
+
+// Functional
+export { Button, type ButtonProps } from './Button/Button';
+export { Select, type SelectProps } from './Select/Select';
